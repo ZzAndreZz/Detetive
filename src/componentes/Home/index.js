13020f00f7998ee36daf832414bfdcc499003eba
Treinamento <Treinamento@RD723918.raiadrogasil.com>
2020-02-28 17:28:03 -0300

    Pronto

---
+++ src/componentes/Home/index.js
@@ -0,0 +1,111 @@
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import {
+    Navbar, Input, Button, InputGroup, InputGroupAddon, Container, Col, Row, Form, Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Spinner
+} from "reactstrap";
+import { MdSearch, MdStar } from "react-icons/md";
+
+
+class Home extends Component {
+    state = {
+        carregando: false,
+        meteoro: []
+    }
+
+    meteoroDaPaixao = async (evento) => {
+        evento.preventDefault()
+
+        this.setState({ carregando: true })
+
+        const form = evento.target;
+        const inputGroup = form.children[0];
+        const input = inputGroup.children[0];
+
+        // const {seguidores: data} = await axios(`https://api.github.com/users/${input.value}/followers`)
+        // this.setState({ seguidores: data })
+
+        // const seguidores = await axios(`https://api.github.com/users/${input.value}/followers`)
+        // this.setState({ seguidores: seguidores.data })
+
+        // const informacoes = await axios(`https://api.bitbucket.org/2.0/users/${input.value}`)
+        // this.setState({ informacoes: informacoes.data })
+        // console.log(informacoes.data)
+
+        const meteoro = await axios(`https://api.nasa.gov/planetary/apod?date=${input.value}&api_key=QVuTFtA9v3xTp70nC5FXTlVuHXfSkfcuShvqJ4LX`).catch()
+        this.setState({ meteoro: [meteoro.data, ...this.state.meteoro], carregando: false })
+        console.log(meteoro.data)
+    }
+
+    render() {
+        return (
+            <>
+                <Navbar color="dark">
+                    <Container className="d-flex justify-content-center">
+                        <img className="rounded-circle border border-white mr-3" width="50" src="https://www.thispersondoesnotexist.com/image" alt="pessoa aleatória" />
+                        <span className="text-white">Logado como <Link className="text-white font-weight-bold ml-3">{this.props.match.params.usuario}</Link></span>
+                    </Container>
+                </Navbar>
+
+                <Navbar color="dark" fixed="bottom">
+                    <Container className="d-flex justify-content-center">
+                        <Col xs="12" md="6">
+                            <Form onSubmit={this.meteoroDaPaixao}>
+                                <InputGroup>
+                                    <Input type="date" />
+                                    <InputGroupAddon addonType="append">
+                                        <Button color="danger">
+                                            {this.state.carregando ? (<Spinner color="light" size="sm" />) : (<MdSearch size="20" />)}
+                                        </Button>
+                                    </InputGroupAddon>
+                                </InputGroup>
+                            </Form>
+                        </Col>
+                    </Container>
+                </Navbar>
+
+                {this.state.carregando ? (
+                    <Container className="h-100 d-flex flex-column justify-content-center align-items-center">
+                        <Spinner color="dark" size="lg" />
+                        <span>Carregando...</span>
+                    </Container>
+                ) : (
+                        <Container className="mt-3 mb-5">
+                            <Row>
+                                {this.state.meteoro.map((meteoro) => (
+                                    <Col className="d-flex" xs="12" md="4">
+                                        <Card className="text-white mb-2" color="dark">
+                                            <CardImg top width="100%" height="30%" src={meteoro.url} alt={meteoro.title} />
+                                            <CardBody>
+                                                <CardTitle className="h3 text-center">{meteoro.title}</CardTitle>
+                                                <CardSubtitle className="text-muted text-center">{meteoro.date.split("-").reverse().join("/")}</CardSubtitle>
+                                                <CardText className="text-justify">{meteoro.explanation}</CardText>
+                                            </CardBody>
+                                        </Card>
+                                    </Col>
+                                ))}
+                            </Row>
+                        </Container>
+                    )}
+
+                {this.state.meteoro.length === 0 && (
+                    <Container className="h-100 d-flex flex-column justify-content-center align-items-center">
+                        <MdStar color="#000" size="150" />
+                        <h3>Escolha uma data KKKKKKK</h3>
+                    </Container>
+                )}
+
+                {/* {this.state.carregando && (
+                    <Container className="h-100 d-flex flex-column justify-content-center align-items-center">
+                        <Spinner color="dark" size="lg" />
+                        <span>Carregando...</span>
+                    </Container>
+                )} */}
+
+            </>
+        )
+    }
+}
+
+export default Home;
